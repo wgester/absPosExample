782c4066bd3bfb7172623ed4748b35f97369be05
Myles Borins <myles.borins@gmail.com>
2014-04-08 15:59:48 -0700

    Add famous logo to base

--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
+/*globals define*/
 define(function(require, exports, module) {
+    'use strict';
     // import dependencies
     var Engine = require('famous/core/Engine');
-   
+    var Surface = require('famous/core/Surface');
+    var Modifier = require('famous/core/Modifier');
+
     // create the main context
     var mainContext = Engine.createContext();
-    
-    
+
+    function SomeFunction() {
+
+    }
+
     // your app here
-      
+    var outline = new Surface({
+        size: [200, 200],
+        content: '<img width="200" src="' + 'src/images/famous_symbol_transparent.png' + '"/>',
+        properties: {
+            lineHeight: '200px',
+            textAlign: 'center'
+        }
+    });
+
+    var outlineModifier = new Modifier({
+        origin: [0.5, 0.5]
+    });
+
+    mainContext.add(outlineModifier).add(outline);
 
 });
