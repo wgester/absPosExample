43e8f0c13a31628192e525a5b14c932f4c3fe00b
Will Gester <willgester@gmail.com>
2014-06-30 13:22:47 -0700

    added basic array methods

--- src/main.js
+++ src/main.js
@@ -3,27 +3,21 @@ define(function(require, exports, module) {
     'use strict';
     // import dependencies
     var Engine = require('famous/core/Engine');
-    var Modifier = require('famous/core/Modifier');
-    var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
+    var Surface = require('famous/core/surface');
+    var CollectionManager = require('famous/core/CollectionManager');
 
-    // create the main context
-    var mainContext = Engine.createContext();
+    var surfaces = [];
+    for (var i = 1; i < 11; i++) {
+        var surface = new Surface({
+            size = [i * 10, i * 10]
+        });
+        surfaces.push(surface);
+    }
+    
+    var cm = new CollectionManager();
+    cm.sequenceFrom(surfaces);
 
-    // your app here
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: './resources/famous_logo.png',
-        classes: ['backfaceVisibility']
-    });
+    window.cm = cm;
 
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform : function(){
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
-        }
-    });
 
-    mainContext.add(centerSpinModifier).add(logo);
 });
